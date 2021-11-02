28b4810c2c4a9dd2fc754ca245547014ff2cf00c
Roy Ho <kwanyin2000@gmail.com>
2021-11-02 17:42:43 +0800

    setup mongoose

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        // useFindAndModify: false
+    })
+
+    console.log(`MongoDB Conencted: ${conn.connection.host}`)
+}
+
+module.exports = connectDB
