190db2fd98d175bda8970e302ef406a4a98122f6
Roy Ho <kwanyin2000@gmail.com>
2021-11-03 00:58:41 +0800

    get and update bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,44 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show all bootcamps` })
+getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
 
 // @desc    Get bootcamp by id
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` })
+getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            res.status(400).json({
+                success: false,
+                message: `Invalid ID - ${req.params.id}`
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false
+        })
+    }
 }
 
 // @desc    Create new bootcamp
@@ -39,8 +68,22 @@ createBootcamp = async (req, res, next) => {
 // @desc    Update bootcamp by id
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-udpateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` })
+udpateBootcamp = async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators: true
+    })
+
+    if (!bootcamp) {
+        return res.status(400).json({
+            success: false
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        data: bootcamp
+    })
 }
 
 // @desc    Delete bootcamp by id
