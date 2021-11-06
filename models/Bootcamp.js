faeb1a716f1e69c69117097a5b48850bc4291b18
Roy Ho <kwanyin2000@gmail.com>
2021-11-06 20:04:13 +0800

    add module 'slugify' and use it on model save to create slug automatically

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -98,6 +99,15 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+})
+
+// Create Bootcamp slug from the name
+BootcampSchema.pre('save', function(next) {
+    // console.log('Slugify ran', this.name)
+
+    this.slug = slugify(this.name, { lower: true })
+
+    next()
 })
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
