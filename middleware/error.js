303b6efdf671df4a3d114fd915dbf16cd4914a89
Roy Ho <kwanyin2000@gmail.com>
2021-11-06 17:52:02 +0800

    custom error handling for error code and validation error

--- middleware/error.js
+++ middleware/error.js
@@ -7,8 +7,21 @@ const errorHandler = (error, req, res, next) => {
 
     console.log(error.stack.red)
 
+    // return error by error name
     if(error.name === 'CastError') {
         const message = `Resource not found with id of ${error.value}`
+        err = new ErrorResponse(message, 404)
+    }
+
+    // return error by error code
+    if(error.code === 11000) {
+        const message = `Duplicate field value entered`
+        err = new ErrorResponse(message, 404)
+    }
+
+    // mongoose validation error
+    if(error.name === 'ValidationError') {
+        const message = Object.values(error.errors).map(val => val.message)
         err = new ErrorResponse(message, 404)
     }
 
