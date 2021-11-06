a2a0fb36ec38539a945449adf9319e774d9a96d9
Roy Ho <kwanyin2000@gmail.com>
2021-11-06 17:51:19 +0800

    fix bootcamp description validation message

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -11,7 +11,7 @@ const BootcampSchema = new mongoose.Schema({
     slug: String,
     description: {
         type: String,
-        required: [true, 'Please add a name'],
+        required: [true, 'Please add a description'],
         maxlength: [500, 'Description can not be more than 500 characters']
     },
     website: {
