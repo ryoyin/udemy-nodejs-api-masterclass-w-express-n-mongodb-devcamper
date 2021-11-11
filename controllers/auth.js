c0002fcc270605788abaec8f02f9c1f2feb4c6da
Roy Ho <kwanyin2000@gmail.com>
2021-11-11 16:16:45 +0800

    add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,3 +21,37 @@ exports.register = asyncHandler(async (req, res, next) => {
         token
     })
 })
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    // validate email and password
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400))
+    }
+
+    // check for user
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user) {
+        return next(new ErrorResponse('Invalid credentials', 400))
+    }
+
+    // check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if(!isMatch) {
+        return next(new ErrorResponse('Invalid password', 400))
+    }
+    
+    // create token
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+})
