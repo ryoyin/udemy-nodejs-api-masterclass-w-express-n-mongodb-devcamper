736f46279da580edf3e276fdba20bd537c92f203
Roy Ho <kwanyin2000@gmail.com>
2021-11-14 01:41:54 +0800

    check course ownership before create update delete

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,6 +2,7 @@ const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const Course = require('../models/Course')
 const Bootcamp = require('../models/Bootcamp')
+const User = require('../models/User')
 
 // @desc    Get all courses
 // @route   GET /api/v1/courses
@@ -46,11 +47,17 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId
+    req.body.user = req.user.id
 
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 400))
+    }
+
+    // check ownership
+    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`You do not have permission to create course under this bootcamp`, 401))
     }
 
     const course = await Course.create(req.body)
@@ -71,10 +78,15 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 400))
     }
 
+    // check ownership
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`You do not have permission to update this course`, 401))
+    }
+
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
-    })
+    })    
 
     res.status(200).json({
         success: true,
@@ -90,6 +102,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
     if (!course) {
         return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 400))
+    }
+
+    // check ownership
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`You do not have permission to delete this course`, 401))
     }
 
     await course.remove()
