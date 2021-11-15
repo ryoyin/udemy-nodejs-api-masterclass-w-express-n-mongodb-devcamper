41a2ffc4dc14965ab0973240bf8110ff762d07e2
Roy Ho <kwanyin2000@gmail.com>
2021-11-16 04:39:10 +0800

    add show all reviews and get review by bootcamp ID

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,6 +6,7 @@ const Bootcamp = require('../models/Bootcamp')
 
 // include other resource router
 const courseRouter = require('./courses')
+const reviewRouter = require('./reviews')
 
 const router = express.Router()
 
@@ -14,6 +15,7 @@ const { protect, authorize } = require('../middleware/auth')
 
 // re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter)
+router.use('/:bootcampId/reviews', reviewRouter)
 
 router
     .route('/radius/:zipcode/:distance')
