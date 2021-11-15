d8160525f94db00308fb3505350b14a36f937cea
Roy Ho <kwanyin2000@gmail.com>
2021-11-16 03:21:23 +0800

    enhance coding

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -3,13 +3,13 @@ const express = require('express')
 const { getBootcamps, getBootcamp, createBootcamp, udpateBootcamp, deleteBootcamp, getBootcampsInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps')
 
 const Bootcamp = require('../models/Bootcamp')
-const advancedResults = require('../middleware/advancedResults')
 
 // include other resource router
 const courseRouter = require('./courses')
 
 const router = express.Router()
 
+const advancedResults = require('../middleware/advancedResults')
 const { protect, authorize } = require('../middleware/auth')
 
 // re-route into other resource routers
@@ -17,21 +17,21 @@ router.use('/:bootcampId/courses', courseRouter)
 
 router
     .route('/radius/:zipcode/:distance')
-    .get(getBootcampsInRadius)
+        .get(getBootcampsInRadius)
 
 router
     .route('/:id/photo')
-    .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
+        .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
 
 router
     .route('/')
-    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(protect, authorize('publisher', 'admin'), createBootcamp)
+        .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+        .post(protect, authorize('publisher', 'admin'), createBootcamp)
 
 router
     .route('/:id')
-    .get(getBootcamp)
-    .put(protect, authorize('publisher', 'admin'), udpateBootcamp)
-    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
+        .get(getBootcamp)
+        .put(protect, authorize('publisher', 'admin'), udpateBootcamp)
+        .delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
 
 module.exports = router
