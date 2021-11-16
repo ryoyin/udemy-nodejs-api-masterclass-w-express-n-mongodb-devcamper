52f2a57f9025a297fcb483cc684bbdf1c8ac1ef0
Roy Ho <kwanyin2000@gmail.com>
2021-11-17 02:36:55 +0800

    prevent XSS script using xss-clean

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors')
 const fileupload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 const router = express.Router()
@@ -40,6 +42,12 @@ app.use(fileupload())
 
 // Sanitize data
 app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS Script
+app.use(xss())
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
