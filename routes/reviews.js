b5eb2d06b1be633ff5dc6067dfb26b5b99ab0c55
Roy Ho <kwanyin2000@gmail.com>
2021-11-16 18:00:47 +0800

    create review for bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,8 @@ const express = require('express')
 
 const { 
     getReviews, 
-    getReview
+    getReview,
+    addReview
 } = require('../controllers/reviews')
 
 const Review = require('../models/Review')
@@ -18,6 +19,7 @@ router
             path: 'bootcamp',
             select: 'name description'
         }), getReviews)
+        .post(protect, authorize('user', 'admin'), addReview)
 
 router
     .route('/:id')
