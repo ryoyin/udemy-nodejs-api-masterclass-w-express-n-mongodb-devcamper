732c7b5a3934890754039ae79c6f17bc67785ebd
Roy Ho <kwanyin2000@gmail.com>
2021-11-17 02:42:56 +0800

    set request limit and prevent http params pollution

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 const router = express.Router()
@@ -48,6 +50,17 @@ app.use(helmet())
 
 // Prevent XSS Script
 app.use(xss())
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 100
+})
+
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
